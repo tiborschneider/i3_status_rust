f612704ce26ea46b24cb01f358a0b226db347adb
Tibor Schneider <tiborschneider@bluewin.ch>
2019-12-08 23:12:54 +0100

    Updated mail block (using correct maildir)

--- src/block/mail.rs
+++ src/block/mail.rs
@@ -4,23 +4,17 @@ use crate::event::Event;
 use std::sync::{Arc, Mutex};
 use std::sync::mpsc::{Sender, Receiver};
 use std::time::Duration;
-// use std::process::Command;
 
-use std::fs::read_dir;
-use std::path::Path;
+use maildir;
 
 const ICON: char = '\u{f6ee}';
-const BLUEWIN_DIR: &str = "/home/tibor/Mail/bluewin/INBOX/new";
-const ETH_DIR: &str = "/home/tibor/Mail/ETH/INBOX/new";
-
-// const MAIL_EMACS_CMD: &str = "\'(mu4e-headers-search-bookmark \"flag:unread and not flag:trashed\")\'";
+const BLUEWIN_DIR: &str = "/home/tibor/Mail/bluewin/INBOX";
+const ETH_DIR: &str = "/home/tibor/Mail/ETH/INBOX";
 
 pub fn mail_loop(elem: Arc<Mutex<Element>>, tx: Sender<i32>, event: Receiver<Event>) {
-    let bluewin_dir = Path::new(BLUEWIN_DIR);
-    let eth_dir = Path::new(ETH_DIR);
     loop {
         let mut new_text = String::with_capacity(6);
-        let unread = (read_dir(&bluewin_dir).unwrap().count() + read_dir(&eth_dir).unwrap().count()) as u32;
+        let unread = count_unread(BLUEWIN_DIR) + count_unread(ETH_DIR);
         new_text.push(ICON);
         new_text.push(' ');
         new_text.push_str(&format!("{}", unread));
@@ -40,18 +34,17 @@ pub fn mail_loop(elem: Arc<Mutex<Element>>, tx: Sender<i32>, event: Receiver<Eve
 
         if updated {
             if tx.send(1).is_err() {
-                eprintln!("(time): cannot communicate to main bar. Abort!");
+                eprintln!("(mail): cannot communicate to main bar. Abort!");
                 break;
             }
         }
 
-        // sleep for one second
-        match event.recv_timeout(Duration::new(0, 100_000_000)) {
-            Ok(_) => {
-                // launch emacs client for mail and read unread
-                //Command::new("emacsclient").arg("-ce").arg(MAIL_EMACS_CMD).spawn().unwrap();
-            },
-            Err(_) => { }
-        }
+        // sleep for one second or until an interrupt occurrs
+        event.recv_timeout(Duration::new(0, 100_000_000)).unwrap();
     }
+}
+
+fn count_unread(maildir_path: &str) -> usize {
+    let _maildir = maildir::Maildir::from(maildir_path);
+    _maildir.count_new()
 }
